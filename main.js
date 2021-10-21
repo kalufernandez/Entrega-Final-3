124b58f9afd4c0c83f56a82ef9c48788b6da2d9c
Lucas Fernandez <lucas.emiliano.fernandez@gmail.com>
2021-10-21 16:03:39 -0300

    Entrega Final 3

--- main.js
+++ main.js
@@ -4,20 +4,26 @@ $("body").prepend('<button id="btn1">Menu</button>');
 
 $("#btn1").click(function() { //Llamo al id del boton creado, despues le digo cual va a ser el evento que lo va a invocar y por ultimo le paso la funcion que se va a ejecutar a partir del evento click. 
     for(repuesto of spares){
-        $(`#btn1`).append(
+        $(`body`).append(
             `<div>
             <h4> Repuesto: ${repuesto.nombre}</h4>
             <h5> Stock: ${repuesto.stock}</h5>
             <button id ="btn3-${repuesto.id}">Mostrar stock</button>
             </div>`);
-    
-        $(`#btn3-${repuesto.id}`).click( function (){
-            console.log(`${repuesto.stock}`)
-        });
-
-        }});
+                    /* console.log(`${repuesto.stock}`) */
 
 
+        $(`#btn3-${repuesto.id}`).click(function(){
+            $(`body`).append(
+                `<div>
+                <h5> Stock: ${repuesto.stock}</h5>
+                </div>`);
+        })  
+        };
+
+       
+     
+        
 
 $(`#btn2`).on("click", function () {
     for (repuesto of spares) {
@@ -31,4 +37,4 @@ $("#mostrarMenu").click(() => {
     $("#btn1").trigger("click");
     $("#btn2").trigger("click");
     document.body.style.backgroundColor = `yellow`
-});
+})});
